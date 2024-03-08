25a4f29802ff2f79d515da74133592147c24b41c
szolton <szolton.designs@gmail.com>
2024-03-08 17:08:05 -0500

    updated colors to automatically show up with the time of day

--- script.js
+++ script.js
@@ -17,132 +17,28 @@ var planWeek = [
     { time: "4:30 PM", event: "" },
     { time: "5:00 PM", event: "" },
   ],
-  [
-    { time: "9:00 AM", event: "" },
-    { time: "9:30 AM", event: "" },
-    { time: "10:00 AM", event: "" },
-    { time: "10:30 AM", event: "" },
-    { time: "11:00 AM", event: "" },
-    { time: "12:00 PM", event: "" },
-    { time: "12:30 PM", event: "" },
-    { time: "1:00 PM", event: "" },
-    { time: "1:30 PM", event: "" },
-    { time: "2:00 PM", event: "" },
-    { time: "2:30 PM", event: "" },
-    { time: "3:00 PM", event: "" },
-    { time: "3:30 PM", event: "" },
-    { time: "4:00 PM", event: "" },
-    { time: "4:30 PM", event: "" },
-    { time: "5:00 PM", event: "" },
-  ],
-  [
-    { time: "9:00 AM", event: "" },
-    { time: "9:30 AM", event: "" },
-    { time: "10:00 AM", event: "" },
-    { time: "10:30 AM", event: "" },
-    { time: "11:00 AM", event: "" },
-    { time: "12:00 PM", event: "" },
-    { time: "12:30 PM", event: "" },
-    { time: "1:00 PM", event: "" },
-    { time: "1:30 PM", event: "" },
-    { time: "2:00 PM", event: "" },
-    { time: "2:30 PM", event: "" },
-    { time: "3:00 PM", event: "" },
-    { time: "3:30 PM", event: "" },
-    { time: "4:00 PM", event: "" },
-    { time: "4:30 PM", event: "" },
-    { time: "5:00 PM", event: "" },
-  ],
-  [
-    { time: "9:00 AM", event: "" },
-    { time: "9:30 AM", event: "" },
-    { time: "10:00 AM", event: "" },
-    { time: "10:30 AM", event: "" },
-    { time: "11:00 AM", event: "" },
-    { time: "12:00 PM", event: "" },
-    { time: "12:30 PM", event: "" },
-    { time: "1:00 PM", event: "" },
-    { time: "1:30 PM", event: "" },
-    { time: "2:00 PM", event: "" },
-    { time: "2:30 PM", event: "" },
-    { time: "3:00 PM", event: "" },
-    { time: "3:30 PM", event: "" },
-    { time: "4:00 PM", event: "" },
-    { time: "4:30 PM", event: "" },
-    { time: "5:00 PM", event: "" },
-  ],
-  [
-    { time: "9:00 AM", event: "" },
-    { time: "9:30 AM", event: "" },
-    { time: "10:00 AM", event: "" },
-    { time: "10:30 AM", event: "" },
-    { time: "11:00 AM", event: "" },
-    { time: "12:00 PM", event: "" },
-    { time: "12:30 PM", event: "" },
-    { time: "1:00 PM", event: "" },
-    { time: "1:30 PM", event: "" },
-    { time: "2:00 PM", event: "" },
-    { time: "2:30 PM", event: "" },
-    { time: "3:00 PM", event: "" },
-    { time: "3:30 PM", event: "" },
-    { time: "4:00 PM", event: "" },
-    { time: "4:30 PM", event: "" },
-    { time: "5:00 PM", event: "" },
-  ],
-  [
-    { time: "9:00 AM", event: "" },
-    { time: "9:30 AM", event: "" },
-    { time: "10:00 AM", event: "" },
-    { time: "10:30 AM", event: "" },
-    { time: "11:00 AM", event: "" },
-    { time: "12:00 PM", event: "" },
-    { time: "12:30 PM", event: "" },
-    { time: "1:00 PM", event: "" },
-    { time: "1:30 PM", event: "" },
-    { time: "2:00 PM", event: "" },
-    { time: "2:30 PM", event: "" },
-    { time: "3:00 PM", event: "" },
-    { time: "3:30 PM", event: "" },
-    { time: "4:00 PM", event: "" },
-    { time: "4:30 PM", event: "" },
-    { time: "5:00 PM", event: "" },
-  ],
-  [
-    { time: "9:00 AM", event: "" },
-    { time: "9:30 AM", event: "" },
-    { time: "10:00 AM", event: "" },
-    { time: "10:30 AM", event: "" },
-    { time: "11:00 AM", event: "" },
-    { time: "12:00 PM", event: "" },
-    { time: "12:30 PM", event: "" },
-    { time: "1:00 PM", event: "" },
-    { time: "1:30 PM", event: "" },
-    { time: "2:00 PM", event: "" },
-    { time: "2:30 PM", event: "" },
-    { time: "3:00 PM", event: "" },
-    { time: "3:30 PM", event: "" },
-    { time: "4:00 PM", event: "" },
-    { time: "4:30 PM", event: "" },
-    { time: "5:00 PM", event: "" },
-  ],
-  
 ];
 
+// prevents redeclaration
 var dayOffset = 0;
 
 function colorRow(time) {
-  var planDate = moment(time, "h:mm A");
-  var currentDate = moment().startOf('hour');
+  var planNow = moment(time, "h:mm A");
+  var currentHour = moment().startOf('hour');
+  var currentTime = moment();
 
-  if (planDate.isBefore(currentDate)) {
+  if (planNow.isBefore(currentTime, 'minute')) {
     return "past";
-  } else if (planDate.isSame(currentDate, 'minute')) {
-    return "present";
+  } else if (planNow.isSame(currentHour, 'hour')) {
+    if (currentTime.isBetween(planNow, planNow.clone().add(30, 'minutes'))) {
+      return "present";
+    } else {
+      return "future";
+    }
   } else {
     return "future";
   }
 }
-
 
 
 function updateColors() {
@@ -150,35 +46,58 @@ function updateColors() {
 
   $(".time-block").each(function(index, element) {
     var timeLabel = planWeek[dayOffset][index].time;
-    var planDate = moment(timeLabel, "h:mm A").startOf('day');
+    var color = colorRow(timeLabel);
 
-    var color = "";
-    if (planDate.isBefore(currentDate)) {
-      color = "past";
-    } else if (planDate.isSame(currentDate, 'day')) {
-      color = "present";
+    if (color === "future") {
+      $(element).css("background-color", "#E2B5F7");
+    } else if (color === "past") {
+      if (moment(timeLabel, "h:mm A").isBefore(currentDate)) {
+        $(element).css("background-color", "#F4BBA2");
+      }
+    } if (color === "present") {
+      $(element).css("background-color", "#C9BAD3");
     } else {
-      color = "future";
+      $(element).css("background-color", "");
     }
-
-    console.log("Time: " + timeLabel + ", Color: " + color);
-    
-    $(element).removeClass("past present future").addClass(color);
   });
 }
 
 
+
+// Update colors every half hour
+setInterval(updateColors, 1800000); // 1800000 milliseconds = 30 minutes
+
+
+// calculates the current date based on the dayOffset variable
 function updateDay() {
   var currentDate = moment().add(dayOffset, 'days');
 
+  // clears the contents of the container element
   $(".container").empty();
 
+let id = 8.5;
+
+// iterates each timeblock in the planweek array for the current dayoffset, and generates HTML elements to display the time blocks in the scheduler
   planWeek[dayOffset].forEach(function(timeBlock, index) {
+    // console.log("time-block", timeBlock);
+    id=id+.5
+    if (index >= 26) {
+  id=9
+
+  }
+
+   
+    // extracts the time property from the timeBlock object
     var timeLabel = timeBlock.time;
+    // calls the colorRow function to determine the background color for current time block
     var blockColor = colorRow(timeLabel);
+   
+    
+
+    // string containing HTML markup for a single time blcok, including the time label, textarea, and save button
     var row =
     `<div class="row time-block ${blockColor}">
-      <div class="col-2 col-md-1 hour text-center py-3">${timeLabel}</div>
+      <div id="${id}" class="col-2 col-md-1 hour text-center py-3">${timeLabel}</div>
       <textarea id="event${index}" class="col-8 col-md-10 description" rows="3">${timeBlock.event}</textarea>
       <button class="btn saveBtn col-2 col-md-1" data-index="${index}" aria-label="save">
         <i class="fas fa-save" aria-hidden="true"></i>
@@ -189,10 +108,11 @@ function updateDay() {
 
   $("#currentDay").text(currentDate.format("dddd, MMM Do YYYY"));
 
-  // Update the timer
+  // updates the timer
   updateTimer();
 
-  updateColors(); // Update colors after rendering the time blocks
+  // updates colors after rendering the time blocks
+  updateColors(); // 
 }
 
 function updateTimer() {
@@ -201,34 +121,36 @@ function updateTimer() {
     var formattedTime = currentTime.format("h:mm:ss A");
     $("#timer").text(formattedTime);
   }, 1000); // Update every second
+
+  
 }
 
+// interactive previous/next buttons
+// $("#prev-btn").on("click", function() {
+//   dayOffset--;
+//   if (dayOffset < 0) {
+//     dayOffset = planWeek.length - 1;
+//   }
+//   console.log("Prev button clicked. Day offset:", dayOffset);
+//   updateDay();
+// });
 
-$("#prev-btn").on("click", function() {
-  dayOffset--;
-  if (dayOffset < 0) {
-    dayOffset = planWeek.length - 1;
-  }
-  console.log("Prev button clicked. Day offset:", dayOffset);
-  updateDay();
-});
-
-$("#next-btn").on("click", function() {
-  dayOffset++;
-  if (dayOffset >= planWeek.length) {
-    dayOffset = 0;
-  }
-  console.log("Next button clicked. Day offset:", dayOffset);
-  updateDay();
-});
+// $("#next-btn").on("click", function() {
+//   dayOffset++;
+//   if (dayOffset >= planWeek.length) {
+//     dayOffset = 0;
+//   }
+//   console.log("Next button clicked. Day offset:", dayOffset);
+//   updateDay();
+// });
 
 
-// Load saved events on page load and update the UI
+// loads saved events on page load and update the UI
 window.onload = function() {
   for (var i = 0; i < planWeek.length; i++) {
     for (var j = 0; j < planWeek[i].length; j++) {
       var eventId = 'event' + j;
-      var event = localStorage.getItem(eventId + '-' + i); // Use a unique identifier for each day
+      var event = localStorage.getItem(eventId + '-' + i); // 
       if (event !== null) {
         planWeek[i][j].event = event;
       }
@@ -237,19 +159,24 @@ window.onload = function() {
   updateDay();
 };
 
+// saves events entered into the text area
+// retreives the value of the textarea eleemnt with the id 'event' + id
 function saveEvent(id) {
   var event = document.getElementById('event' + id).value;
-  localStorage.setItem('event' + id + '-' + dayOffset, event); // Use a unique identifier for each day
+  // used to save the event to the browsers local storage
+  localStorage.setItem('event' + id + '-' + dayOffset, event); 
+  // updated with the new event value, ensures that the planWeek array stays in sync w local storage
   planWeek[dayOffset][id].event = event;
 }
 
+// uses the jQuery to attach an event listener to all textarea elements within the container eleement
 $(".container").on("input", "textarea", function() {
   var index = $(this).attr('id').replace('event', '');
   saveEvent(index);
 });
 
-// Update colors every minute
-setInterval(updateColors, 60000); // 60000 milliseconds = 1 minute
+// updated colors every minute
+setInterval(updateColors, 60000);
 
-// Initial update of the day
+// initial update of the day
 updateDay();
