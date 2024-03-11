5835b4d12c343175a5022d5e15a85653f02fcb3b
szolton <szolton.designs@gmail.com>
2024-03-11 17:25:51 -0400

    commented out the javascript, index, and css more thoroughly

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// day of the week and time
 var planWeek = [
   [
     { time: "9:00 AM", event: "" },
@@ -24,6 +25,7 @@ var dayOffset = 0;
 
 // function to determine the color of a time block based on the current time
 function colorRow(time) {
+ 
   // gets the current time
   var currentTime = moment();
 
@@ -32,9 +34,11 @@ function colorRow(time) {
 
 // checks if the current time is within the same hour and 30 minutes of the time block
   if (currentTime.isSame(planNow, 'hour') && currentTime.isBetween(planNow, planNow.clone().add(30, 'minutes'))) {
+  
     // return 'present' if the current time is within the time block
     return "present";
   } else if (planNow.isBefore(currentTime)) {
+   
     // return 'future' if the time block is in the future
     return "past";
   } else {
@@ -44,23 +48,31 @@ function colorRow(time) {
 
 // Function to update colors based on current time
 function updateColors() {
+ 
+  // this gets the current hour
   var currentHour = moment().startOf('hour');
 
+  // this loops through each time block elemenet
   $(".time-block").each(function(index, element) {
+    // this gets the time label for the current time block
     var timeLabel = planWeek[0][index].time;
+    
+    // this parses the time label into a moment object
     var planNow = moment(timeLabel, "h:mm A");
 
+    // this checks if the current time block is in the current hour and within the next 30 minutes
     if (planNow.isSame(currentHour, 'hour')) {
       if (moment().isBetween(planNow, planNow.clone().add(30, 'minutes'))) {
         $(element).css("background-color", "#F4E1F3"); // Present time
+        // this exits the loop early if the condition is met
         return;
       }
     }
 
     if (planNow.isBefore(moment(), 'minute')) {
-      $(element).css("background-color", "#F4BBA2"); // Past time
+      $(element).css("background-color", "#F4BBA2"); // past time
     } else {
-      $(element).css("background-color", "#E2B5F7"); // Future time
+      $(element).css("background-color", "#E2B5F7"); // future time
     }
   });
 }
@@ -72,20 +84,26 @@ setInterval(updateColors, 1800000); // 1800000 milliseconds = 30 minutes
 // initial update of the day
 updateColors();
 
-
+// function to update the colors of the time blocks based on the current time
 function updateColorsBasedOnCurrentTime() {
   $(".time-block").each(function(index, element) {
+
+    // gets the time label for the current time block
     var timeLabel = planWeek[dayOffset][index].time;
+
+    // this gets the color for the current time block
     var color = colorRow(timeLabel);
 
+
     if (color === "present") {
-      $(element).css("background-color", "#F4E1F3"); // Set background color to #F4E1F3 for present time
+      $(element).css("background-color", "#F4E1F3"); // sets the background color to #F4E1F3 for present time blocks
     } else {
+
+      // removes any background color if not the present time
       $(element).css("background-color", "");
     }
   });
 }
-
 
 // calculates the current date based on the dayOffset variable
 function updateDay() {
@@ -98,7 +116,11 @@ function updateDay() {
 
   // iterates each timeblock in the planweek array for the current dayoffset, and generates HTML elements to display the time blocks in the scheduler
   planWeek[dayOffset].forEach(function(timeBlock, index) {
+   
+    // increment id by .5 for each iteration
     id = id + .5
+   
+    // resets the id to 9 if the index is greater than or equal to 26
     if (index >= 26) {
       id = 9
     }
@@ -106,6 +128,7 @@ function updateDay() {
     var timeLabel = timeBlock.time;
     var blockColor = colorRow(timeLabel);
 
+    // constructs the HTML for a time block row
     var row =
     `<div class="row time-block ${blockColor}">
       <div id="${id}" class="col-2 col-md-1 hour text-center py-3">${timeLabel}</div>
@@ -114,9 +137,12 @@ function updateDay() {
         <i class="fas fa-save" aria-hidden="true"></i>
       </button>
     </div>`;
+
+    // this appends the row to the container element
     $(".container").append(row);
   });
 
+  // this updates the text of the currentDay element to display the current date
   $("#currentDay").text(currentDate.format("dddd, MMM Do YYYY"));
 
   // updates the timer
@@ -129,41 +155,64 @@ function updateDay() {
   updateColorsBasedOnCurrentTime();
 }
 
-var timerInterval; // Define a variable to hold the interval
+ // defines a variable to hold the interval
+var timerInterval;
 
 function updateTimer() {
-  clearInterval(timerInterval); // Clear the previous interval
+  clearInterval(timerInterval); // this clears the previous interval
   timerInterval = setInterval(function() {
     var currentTime = moment();
     var formattedTime = currentTime.format("h:mm:ss A");
+ 
+    //  this updates the #timer element with the formatted time
     $("#timer").text(formattedTime);
-  }, 1000); // Update every second
+  }, 1000); // updates every second
 }
 
 // loads saved events on page load and update the UI
 window.onload = function() {
+ 
+  // loop through each day in planWeek
   for (var i = 0; i < planWeek.length; i++) {
+   
+    // loops through each time block in the current day
     for (var j = 0; j < planWeek[i].length; j++) {
       var eventId = 'event' + j;
+   
+      //  pulls the saved event from localStorage
       var event = localStorage.getItem(eventId + '-' + i);
       if (event !== null) {
+
+        // updates the event in the planWeek array
         planWeek[i][j].event = event;
       }
     }
   }
+
+  // this updates the day UI
   updateDay();
 };
 
 // saves events entered into the text area
 function saveEvent(id) {
+
+  // this gets the value of the textarea with the corresponding ID
   var event = document.getElementById('event' + id).value;
+
+  // this saves the event to localStorage
   localStorage.setItem('event' + id + '-' + dayOffset, event);
+
+  // this updates the event in the planWeek array
   planWeek[dayOffset][id].event = event;
 }
 
 // attach an event listener to all textarea elements within the container element
 $(".container").on("input", "textarea", function() {
+
+  // this gets the index of the textarea from it's id
   var index = $(this).attr('id').replace('event', '');
+  // this calls the saveEvent function with the index
+
   saveEvent(index);
 });
 
